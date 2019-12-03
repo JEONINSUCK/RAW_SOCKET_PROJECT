a767befc8181efd3f51253ec7d2e2df7647dd577
jeoninsuck <wdt0818@naver.com>
2019-12-03 19:37:01 +0900

    icmp_raw send complete

--- ICMP_RAW/icmp_raw.c
+++ ICMP_RAW/icmp_raw.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <netinet/ip.h> // struct ip
+#include <netinet/ip_icmp.h>    // sturct icmp
 #include <netinet/in.h> // IPPROTO_ICMP
 #include <arpa/inet.h>
 
@@ -18,18 +20,36 @@ int main(int argc, char *argv[])
 {
     struct ip ip_hdr;
     struct icmp icmp_hdr;
+    struct sockaddr_in sin;
 
-    // if(argc < 3)
-    // {
-    //     printf("icmp_raw <INTERFACE> <DST_IP>\n");
-    //     exit(EXIT_FAILURE);
-    // }
+    int sock;
+    int on = 1;
+    uint8_t *packet;
+
+    if(argc < 3)
+    {
+        printf("icmp_raw <INTERFACE> <DST_IP>\n");
+        exit(EXIT_FAILURE);
+    }
 
     /* ip header set */
-    ip_init(&ip_hdr, "ens33", "172.16.255.1");
+    ip_init(&ip_hdr, argv[1], argv[2]);
+    icmp_init(&icmp_hdr);
+    
+    memcpy(packet, &ip_hdr, IP_HDRLEN);
+    memcpy((packet + IP_HDRLEN), &icmp_hdr, ICMP_HDRLEN);
 
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = ip_hdr.ip_dst.s_addr;
 
-    // checksum(&ip_hdr, 20);
+    if((sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW)) < 0 )
+        error_handler("socket making error");
+
+    if(setsockopt(sock, IPPROTO_IP, IP_HDRINCL, &on, sizeof(on)) < 0)
+        error_handler("setsockopt() error in main method");
+
+    if(sendto(sock, packet, IP_HDRLEN + ICMP_HDRLEN, 0, (struct sockaddr *) &sin, sizeof(struct sockaddr)) < 0)
+        error_handler("sendto() error in main method");
 
     return 0;
 }
@@ -40,6 +60,7 @@ int ip_init(struct ip *ip_p, char *if_name, char *dst_ip)
         printf("ip_init() start\n");
 
     char *src_ip_buf;
+    char *dst_ip_buf;
     int if_index;
     int checksum;
 
@@ -49,23 +70,23 @@ int ip_init(struct ip *ip_p, char *if_name, char *dst_ip)
     ip_p->ip_v = 4;
     ip_p->ip_hl = IP_HDRLEN / 4; // ip header length device 32bit(20>>2=5)
     ip_p->ip_tos = 0;
-    ip_p->ip_len = htons(IP_HDRLEN + ICMP_HDRLEN + 4);
+    ip_p->ip_len = htons(IP_HDRLEN + ICMP_HDRLEN);
     ip_p->ip_id = htons(0); 
     ip_p->ip_off = htons(0);
     ip_p->ip_ttl = 255;
     ip_p->ip_p = IPPROTO_ICMP;
 
-    if (dns_to_ip("google.com", &dst_ip) == -1)
+    if (dns_to_ip(dst_ip, &dst_ip_buf) == -1)
         return -1;
     if ((inet_pton(AF_INET, src_ip_buf, &(ip_p->ip_src))) != 1)
         return -1;
-    if ((inet_pton(AF_INET, dst_ip, &(ip_p->ip_dst))) != 1)
+    if ((inet_pton(AF_INET, dst_ip_buf, &(ip_p->ip_dst))) != 1)
         return -1;
 
     ip_p->ip_sum = 0;
-    ip_p->ip_sum = check_sum((uint16_t *) &iphdr, IP_HDRLEN);
+    ip_p->ip_sum = check_sum((uint16_t *) ip_p, IP_HDRLEN);
 
-    
+    return 0;    
 }
 
 void error_handler(char *msg)
@@ -76,7 +97,7 @@ void error_handler(char *msg)
 
 uint16_t check_sum(uint16_t *iphdr, int len)
 {
-    int count = 20;
+    int count = len;
     uint32_t sum = 0;
     uint16_t answer = 0;
 
@@ -101,11 +122,15 @@ uint16_t check_sum(uint16_t *iphdr, int len)
 
 int icmp_init(struct icmp *icmp_p)
 {
+    if(DEBUGMODE == 1)
+        printf("icmp_init() start\n");
+
     icmp_p->icmp_type = ICMP_ECHO;
     icmp_p->icmp_code = 0;
     icmp_p->icmp_id = htons(1000);
     icmp_p->icmp_seq = htons(0);
-    imcp_p->icmp_cksum = 0;
+    icmp_p->icmp_cksum = 0;
+    icmp_p->icmp_cksum = check_sum((uint16_t *)icmp_p, ICMP_HDRLEN);
 
-    
+    return 0;
 }
--- ICMP_RAW/test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-
-int main()
-{
-    printf("%d\n", (int)sizeof(int));
-    printf("%d\n", (int)sizeof(uint16_t));
-        
-}
